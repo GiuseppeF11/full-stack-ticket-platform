758eddb02dd8c011cfaf13cc60e2756b7b5f5671
jacopo-derrico <derricoj.design@gmail.com>
2024-05-20 18:12:29 +0200

    create Operator model-migration-seeder

---
+++ database/migrations/2024_05_20_152445_create_operators_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('operators', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->tinyInteger('is_busy')->unsigned()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('operators');
+    }
+};
